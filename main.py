9a6c9831f91ca57bfdc7807cf76e057914eb1b08
Reynold Yehezkiel <reynoldyehezkiel25@gmail.com>
2020-11-24 19:41:54 +0700

    update

--- main.py
+++ main.py
@@ -1,11 +1,42 @@
-import telebot
+import requests as requests
 
-bot = telebot.TeleBot('1453757504:AAENyCN0g_lqFfuaHL7gnJ84TPUZX95liHM')
+url = "https://api.telegram.org/bot1453757504:AAENyCN0g_lqFfuaHL7gnJ84TPUZX95liHM/"
 
-@bot.message_handler(commands=['start'])
-def start(message):
-    bot.reply_to(message, 'Hello')
+# get chat id
+def getChatId(update):
+	chat_id = update["message"]["chat"]["id"]
+	return chat_id
 
+# get message text
+def getMessageText(update):
+	message_text = update["message"]["text"]
+	message_text.lower()
+	return message_text
 
-print('bot is running')
-bot.polling()
+# get last update
+def lastUpdate(req):
+	response = requests.get(req + "getUpdates")
+	response = response.json()
+	result = response["result"]
+	total_updates = len(result) - 1
+	return result[total_updates]	# get last record messsage update
+
+# send message
+def sendMessage(chat_id, message_text):
+	params = {"chat_id": chat_id, "text": message_text}
+	response = requests.post(url + "sendMessage", data=params)
+	return response
+
+# main
+def main():
+	update_id = lastUpdate(url)["update_id"]
+	while True:
+		update = lastUpdate(url)
+		if update_id == update["update_id"]:
+			if getMessageText(update) == "hi" or getMessageText(update) == "halo":
+				sendMessage(getChatId(update), "halo juga")
+			else:
+				sendMessage(getChatId(update), "Maaf, aku belum mengerti maksud kamu")
+			update_id += 1
+
+main()
